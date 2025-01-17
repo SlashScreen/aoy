3973507a7cbf638fa3d3dd15d2758757ab2bd4dd
Amadeus Folego <amadeusfolego@gmail.com>
2025-01-16 21:42:11 -0300

    Cursor works

--- LuaRules/draw.lua
+++ LuaRules/draw.lua
@@ -1,16 +1,1 @@
-local commandCursors = {
-	Attack = "attack",
-	Move = "move",
-}
-
-Spring.AssignMouseCursor("", "cursordefend", true)
-
-for commandName, cursor in pairs(commandCursors) do
-	if Spring.AssignMouseCursor(commandName, "cursor" .. cursor, true) then
-		Spring.Echo("DID ASSIGN", commandName, "cursor" .. cursor)
-	else
-		Spring.Echo("DIDNT ASSIGN", commandName, "cursor" .. cursor)
-	end
-end
---
 VFS.Include("LuaGadgets/gadgets.lua", nil, VFS.BASE)
