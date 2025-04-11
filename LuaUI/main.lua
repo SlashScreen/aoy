646ca36c17b4917068b826dd92255258c82d83e9
Slashscreen <SlashScreen@users.noreply.github.com>
2025-04-10 23:38:20 -0700

    added an opinionated formatter

--- LuaUI/main.lua
+++ LuaUI/main.lua
@@ -15,9 +15,9 @@ Spring.Echo("IM LUAUI aehoo")
 LUAUI_DIRNAME = "LuaUI/"
 LUAUI_VERSION = "LuaUI v0.3"
 
-Spring.SendCommands({"ctrlpanel " .. LUAUI_DIRNAME .. "ctrlpanel.txt"})
+Spring.SendCommands({ "ctrlpanel " .. LUAUI_DIRNAME .. "ctrlpanel.txt" })
 
-VFS.Include(LUAUI_DIRNAME .. 'utils.lua', utilFile)
+VFS.Include(LUAUI_DIRNAME .. "utils.lua", utilFile)
 
 WG = {}
 Spring.Utilities = {}
@@ -28,28 +28,27 @@ include("savetable.lua")
 
 include("debug.lua")
 include("fonts.lua")
-include("layout.lua")   -- contains a simple LayoutButtons()
-include("widgets.lua")  -- the widget handler
+include("layout.lua") -- contains a simple LayoutButtons()
+include("widgets.lua") -- the widget handler
 
 --------------------------------------------------------------------------------
 --
 -- print the header
 --
 
-if (RestartCount == nil) then
-  RestartCount = 0
-else 
-  RestartCount = RestartCount + 1
+if RestartCount == nil then
+	RestartCount = 0
+else
+	RestartCount = RestartCount + 1
 end
 
 do
-  local restartStr = ""
-  if (RestartCount > 0) then
-    restartStr = "  (" .. RestartCount .. " Restarts)"
-  end
-  Spring.SendCommands({"echo " .. LUAUI_VERSION .. restartStr})
+	local restartStr = ""
+	if RestartCount > 0 then
+		restartStr = "  (" .. RestartCount .. " Restarts)"
+	end
+	Spring.SendCommands({ "echo " .. LUAUI_VERSION .. restartStr })
 end
-
 
 --------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
@@ -58,9 +57,8 @@ end
 --
 
 function Say(msg)
-  Spring.SendCommands({'say ' .. msg})
+	Spring.SendCommands({ "say " .. msg })
 end
-
 
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
@@ -72,22 +70,20 @@ activePage = 0
 
 forceLayout = true
 
-
 function Update()
-  local currentPage = Spring.GetActivePage()
-  if (forceLayout or (currentPage ~= activePage)) then
-    Spring.ForceLayoutUpdate()  --  for the page number indicator
-    forceLayout = false
-  end
-  activePage = currentPage
+	local currentPage = Spring.GetActivePage()
+	if forceLayout or (currentPage ~= activePage) then
+		Spring.ForceLayoutUpdate() --  for the page number indicator
+		forceLayout = false
+	end
+	activePage = currentPage
 
-  fontHandler.Update()
+	fontHandler.Update()
 
-  widgetHandler:Update()
+	widgetHandler:Update()
 
-  return
+	return
 end
-
 
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
@@ -96,63 +92,61 @@ end
 --
 
 function Shutdown()
-  return widgetHandler:Shutdown()
+	return widgetHandler:Shutdown()
 end
 
 function ConfigureLayout(command)
-  return widgetHandler:ConfigureLayout(command)
+	return widgetHandler:ConfigureLayout(command)
 end
 
 function CommandNotify(id, params, options)
-  return widgetHandler:CommandNotify(id, params, options)
+	return widgetHandler:CommandNotify(id, params, options)
 end
 
 function DrawScreen(vsx, vsy)
-  widgetHandler:SetViewSize(vsx, vsy)
-  return widgetHandler:DrawScreen()
+	widgetHandler:SetViewSize(vsx, vsy)
+	return widgetHandler:DrawScreen()
 end
 
 function KeyPress(key, mods, isRepeat, label, unicode)
-  return widgetHandler:KeyPress(key, mods, isRepeat, label, unicode)
+	return widgetHandler:KeyPress(key, mods, isRepeat, label, unicode)
 end
 
 function KeyRelease(key, mods, label, unicode)
-  return widgetHandler:KeyRelease(key, mods, label, unicode)
+	return widgetHandler:KeyRelease(key, mods, label, unicode)
 end
 
 function MouseMove(x, y, dx, dy, button)
-  return widgetHandler:MouseMove(x, y, dx, dy, button)
+	return widgetHandler:MouseMove(x, y, dx, dy, button)
 end
 
 function MousePress(x, y, button)
-  return widgetHandler:MousePress(x, y, button)
+	return widgetHandler:MousePress(x, y, button)
 end
 
 function MouseRelease(x, y, button)
-  return widgetHandler:MouseRelease(x, y, button)
+	return widgetHandler:MouseRelease(x, y, button)
 end
 
 function IsAbove(x, y)
-  return widgetHandler:IsAbove(x, y)
+	return widgetHandler:IsAbove(x, y)
 end
 
 function GetTooltip(x, y)
-  return widgetHandler:GetTooltip(x, y)
+	return widgetHandler:GetTooltip(x, y)
 end
 
 function AddConsoleLine(msg, priority)
-  return widgetHandler:AddConsoleLine(msg, priority)
+	return widgetHandler:AddConsoleLine(msg, priority)
 end
 
 function GroupChanged(groupID)
-  return widgetHandler:GroupChanged(groupID)
+	return widgetHandler:GroupChanged(groupID)
 end
-
 
 --
 -- The unit (and some of the Draw) call-ins are handled
 -- differently (see LuaUI/widgets.lua / UpdateCallIns())
 --
-
 
 --------------------------------------------------------------------------------
