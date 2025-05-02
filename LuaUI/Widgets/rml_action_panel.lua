296a6f8aace8385dcb7b4d10a67caa4051f718a3
Slashscreen <SlashScreen@users.noreply.github.com>
2025-05-01 21:48:48 -0700

    Menu is functional, but needs usability

--- LuaUI/Widgets/rml_action_panel.lua
+++ LuaUI/Widgets/rml_action_panel.lua
@@ -20,13 +20,36 @@ local document
 widget.rmlContext = nil
 
 local BUTTON_LEFT_MOUSE = 0
+local BUTTON_RIGHT_MOUSE = 2
 local MAX_COLUMNS_PER_ROW = 4
 local MAX_ROWS = 3
 
 -- this can be overwritten later to change what code exampleEventHook calls
-local eventCallback = function(ev, row, column)
+local eventCallback = function(ev, id)
+	--[[ for k, v in pairs(ev.parameters) do
+		Spring.Echo(k .. ": " .. v)
+	end ]]
 	if ev.parameters.button == BUTTON_LEFT_MOUSE then
-		Spring.Echo("Clicked row " .. row .. " column " .. column)
+		local index = Spring.GetCmdDescIndex(id)
+		if index == nil then
+			Spring.Echo("uh oh sisters")
+			return
+		end
+		Spring.Echo("Clicked id " .. id .. " index " .. index)
+		--local x = ev.parameters.mouse_x --- @type integer
+		--local y = ev.parameters.mouse_y --- @type integer
+		local button = ev.parameters.button --- @type integer
+
+		Spring.SetActiveCommand(
+			index,
+			button,
+			button == BUTTON_LEFT_MOUSE,
+			button == BUTTON_RIGHT_MOUSE,
+			ev.parameters.alt_key == 1,
+			ev.parameters.ctrl_key == 1,
+			ev.parameters.meta_key == 1,
+			ev.parameters.shift_key == 1
+		)
 	end
 end
 
@@ -36,9 +59,8 @@ end
 
 --- @class ActionButton
 --- @field name string
---- @field row integer
---- @field column integer
 --- @field visible boolean
+--- @field id integer
 
 local dm_handle --- @type Model
 local DATA_MODEL_NAME = "action_panel_model"
@@ -48,22 +70,22 @@ local init_model = {
 
 	actions = {
 		{
-			{ name = "test 1,1", row = 1, column = 1, visible = true },
-			{ name = "test 1,2", row = 1, column = 2, visible = true },
-			{ name = "test 1,3", row = 1, column = 3, visible = true },
-			{ name = "test 1,4", row = 1, column = 4, visible = false },
+			{ name = "test 1,1", visible = true, id = 0 },
+			{ name = "test 1,2", visible = true, id = 0 },
+			{ name = "test 1,3", visible = true, id = 0 },
+			{ name = "test 1,4", visible = false, id = 0 },
 		},
 		{
-			{ name = "test 2,1", row = 2, column = 1, visible = true },
-			{ name = "test 2,2", row = 2, column = 2, visible = true },
-			{ name = "test 2,3", row = 2, column = 3, visible = true },
-			{ name = "test 2,4", row = 1, column = 4, visible = false },
+			{ name = "test 2,1", visible = true, id = 0 },
+			{ name = "test 2,2", visible = true, id = 0 },
+			{ name = "test 2,3", visible = true, id = 0 },
+			{ name = "test 2,4", visible = false, id = 0 },
 		},
 		{
-			{ name = "test 3,1", row = 3, column = 1, visible = true },
-			{ name = "test 3,2", row = 3, column = 2, visible = true },
-			{ name = "test 3,3", row = 3, column = 3, visible = true },
-			{ name = "test 3,4", row = 1, column = 4, visible = false },
+			{ name = "test 3,1", visible = true, id = 0 },
+			{ name = "test 3,2", visible = true, id = 0 },
+			{ name = "test 3,3", visible = true, id = 0 },
+			{ name = "test 3,4", visible = false, id = 0 },
 		},
 	},
 }
@@ -87,13 +109,25 @@ local needs_update = false
 local unit_commands --- @type UnitOrder[]
 
 local function clear_menu()
-	for r, row in pairs(dm_handle.actions) do
-		for a, value in pairs(row) do
+	for _, row in pairs(dm_handle:__GetTable().actions) do
+		for _, value in pairs(row) do
 			value.visible = false
-			Spring.Echo("keys are " .. r .. ", " .. a)
 		end
 	end
+	dm_handle:__SetDirty("actions")
 	Spring.Echo("menu cleared")
+end
+
+local function layoutHandler(xIcons, yIcons, cmdCount, commands)
+	unit_commands = commands
+
+	widgetHandler:CommandsChanged()
+
+	return "", xIcons, yIcons, {}, {}, {}, {}, {}, {}, {}, { [1337] = 9001 }
+end
+
+local function OverrideDefaultMenu()
+	widgetHandler:ConfigLayoutHandler(layoutHandler)
 end
 
 function widget:Initialize()
@@ -104,6 +138,8 @@ function widget:Initialize()
 
 	document = widget.rmlContext:LoadDocument("LuaUi/Widgets/hud/action_panel.rml", widget)
 	assert(document ~= nil, "Failed to load document")
+
+	OverrideDefaultMenu()
 
 	RmlUi.SetDebugContext(widget.whInfo.name)
 	document:ReloadStyleSheet()
@@ -125,11 +161,13 @@ end
 
 function widget:Update()
 	if not needs_update then
-		clear_menu()
 		return
 	end
 
 	Spring.Echo("Commands Changed")
+
+	clear_menu()
+
 	local commands = unit_commands
 	local cmd_count = #commands
 
@@ -139,7 +177,9 @@ function widget:Update()
 	end
 	Spring.Echo(debug_text)
 
-	local actions = dm_handle.actions
+	local actions = dm_handle
+		:__GetTable()--[[@as Model]]
+		.actions
 
 	local function loop()
 		for row = 1, MAX_ROWS do
@@ -149,27 +189,23 @@ function widget:Update()
 					return
 				end
 
-				Spring.Echo(i .. " " .. row .. ", " .. column)
-
 				local command = commands[i] --- @type UnitOrder
+
+				local debug_str = ""
+				for k, v in pairs(command) do
+					debug_str = debug_str .. tostring(k) .. ": " .. tostring(v) .. ", "
+				end
+				Spring.Echo(debug_str)
+
 				local entry = actions[row][column]
 
 				entry.name = command.name
 				entry.visible = not (command.hidden or command.disabled)
-
-				Spring.Echo(
-					"Set entry "
-						.. row
-						.. ", "
-						.. column
-						.. " name: "
-						.. command.name
-						.. " visible: "
-						.. tostring(entry.visible)
-				)
+				entry.id = command.id
 			end
 		end
 	end
 	loop()
+	dm_handle:__SetDirty("actions")
 	needs_update = false
 end
